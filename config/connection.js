37f43bdbc20d32860fc37b18d4e0a9c73a469fb2
Ronak Shah <ronakshah2013@u.northwestern.edu>
2018-02-14 18:14:03 -0600

    Environment vars setup, db setup

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config({
+    path: "../.env"
+});
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'burgers_db'
+});
+
+module.exports = {
+    connection
+};
---
+++ config/orm.js
@@ -0,0 +1,3 @@
+const {connection} = require('./connection');
+
+console.log(connection);
