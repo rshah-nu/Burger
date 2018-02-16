1918da7b8821e30f357a79c91993ab92a7d4da12
Ronak Shah <ronakshah2013@u.northwestern.edu>
2018-02-16 17:55:42 -0600

    Functional put / post request, working on redirect issue for put

--- controllers/burgers_api.js
+++ controllers/burgers_api.js
@@ -12,17 +12,21 @@ router.get('/all', (req, res) => {
 router.post('/new', (req, res) => {
     var userBurger = req.body.burgerName;
     burgerCommands.insertOneBurger(userBurger, (results) => {
-        res.json(results);
+        // res.json(results)
+        res.redirect('/')
     });
 });
 
 router.put('/modify', (req, res) => {
+    console.log('Route hit!');
     var devoured = req.body.devoured;
     var burgerID = req.body.burgerID;
     burgerCommands.updateOneBurger(devoured, burgerID, (results) => {
-        res.json(results);
+        return res.redirect('/')
     });
+
 });
+
 
 module.exports = {
     router
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
     console.log("ready!");
 
-    $(".btn").on("click", function(e) {
+
+    $(".devourBtn").click(function(e) {
         e.preventDefault();
+        console.log('You Clicked me!');
         var devourObject = {
             "burgerID": $(this).attr("burgerID"),
             "devoured": 1
         };
         $.ajax({
-            type: "PUT",
+            type: 'PUT',
             url: '/api/modify',
-            data: devourObject,
+            data: devourObject
         });
     });
+    // $("#devourBtn").on("click", function(e) {
+    //     console.log('Clicked!');
 
+    //     $.ajax({
+    //         type: "PUT",
+    //         url: '/api/modify',
+    //         data: devourObject,
+    //     });
+    //     e.preventDefault();
+    // });
 });
+
